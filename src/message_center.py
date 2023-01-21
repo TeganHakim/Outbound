45dc4b9f6517015d382a41ec957c42f330dab815
TeganHakim <67878686+TeganHakim@users.noreply.github.com>
2023-01-21 13:48:16 -0500

    reminder

--- src/message_center.py
+++ src/message_center.py
@@ -643,7 +643,7 @@ class MessageCenter(customtkinter.CTkFrame):
         LOW_CREDITS = 1000
         if self.credits_remaining <= LOW_CREDITS:
             URL = "https://Outbound-Server.teganhakim.repl.co"
-            API_HEADER = "/api/v1/email"
+            API_HEADER = "/api/v1/reminder"
             reminder = {
                 "api_key": os.getenv("OUTBOUND_API_KEY"),
                 "subject": "Outbound - Low Credits Remaining",
